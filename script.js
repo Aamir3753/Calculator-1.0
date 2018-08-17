391c50d3756100904fc92c7c6aa92f2a46217217
Aamir3753 <37248118+Aamir3753@users.noreply.github.com>
2018-08-17 21:35:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
+    var x = Math.floor(Math.random()*13);
+    console.log(x);
+    document.getElementById("img").setAttribute("src","./images/"+x+".jpeg");
+
 var input, output, firstValue, secondValue, op;
 
 input = document.getElementById("input-box");
@@ -6,8 +10,8 @@ output = document.getElementById("output-box");
 function sClear() {
     op = input.value = output.value = "";
 }
-function del(){
-     input.value = input.value.substring(0,input.value.length-1);
+function del() {
+    input.value = input.value.substring(0, input.value.length - 1);
 }
 function cal(id) {
     input.value = input.value + document.getElementById(id).value;
@@ -46,3 +50,72 @@ function ok() {
     }
 }
 
+document.onkeypress = function (e) {
+    var x = e.which || e.keyCode;
+    if(x===8){
+        
+    }
+    switch (x) {
+        case 48:
+            document.getElementById("0").click();
+            break;
+        case 49:
+            document.getElementById("1").click();
+            break;
+        case 50:
+            document.getElementById("2").click();
+            break;
+        case 51:
+            document.getElementById("3").click();
+            break;
+        case 52:
+            document.getElementById("4").click();
+            break;
+        case 53:
+            document.getElementById("5").click();
+            break;
+        case 54:
+            document.getElementById("6").click();
+            break;
+        case 55:
+            document.getElementById("7").click();
+            break;
+        case 56:
+            document.getElementById("8").click();
+            break;
+        case 57:
+            document.getElementById("9").click();
+            break;
+        case 46:
+            document.getElementById("period").click();
+            break;
+        case 27:
+            document.getElementById("clear").click();
+            break;
+        case 8:
+           document.getElementById("del").click();
+            break;
+        case 47:
+            document.getElementById("/").click();
+            break;
+        case 42:
+            document.getElementById("*").click();
+            break;
+        case 43:
+            document.getElementById("+").click();
+            break;
+        case 42:
+            document.getElementById("*").click();
+            break;
+        case 45:
+            document.getElementById("-").click();
+            break;
+        case 13:
+            document.getElementById("ok").click();
+            break;
+        case 32:
+            document.getElementById("ok").click();
+            break;
+
+    }
+}
